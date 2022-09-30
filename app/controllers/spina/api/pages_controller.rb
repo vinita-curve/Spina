a70e0b9537a8fb79a650bb06359c805a75fa439c
Vinita Kumari <vinita@curvetomorrow.com.au>
2022-09-30 12:47:42 +0530

    modified index action

--- app/controllers/spina/api/pages_controller.rb
+++ app/controllers/spina/api/pages_controller.rb
@@ -2,25 +2,38 @@ module Spina
   module Api
     class PagesController < ApiController
       include Paginable
-      
+
       before_action :set_resource
-      
+
       def index
-        pages = Page.live.includes(:translations).where(resource: @resource).order(:created_at)
-        render json: Spina::Api::PageSerializer.new(*pagination(pages)).serializable_hash.to_json
+        paginated_records = Page.live.includes(:translations).where(
+          resource: @resource
+        ).order(:created_at)
+                                .page(params[:page]).per(params[:per_page])
+        render json: data(paginated_records)
       end
-      
+
       def show
         @page = Page.live.where(resource: @resource).find(params[:id])
         render json: Spina::Api::PageSerializer.new(@page).serializable_hash.to_json
       end
-      
-      private
-      
-        def set_resource
-          @resource = Spina::Resource.find(params[:resource_id]) if params[:resource_id].present?
-        end
 
+      private
+
+      def set_resource
+        @resource = Spina::Resource.find(params[:resource_id]) if params[:resource_id].present?
+      end
+
+      def data(paginated_records)
+        Spina::Api::PageSerializer.new(
+          paginated_records,
+          {
+            meta: pagination_meta(paginated_records),
+            links: pagination_links(paginated_records),
+            params: { view_context: view_context }
+          }
+        ).serializable_hash.to_json
+      end
     end
   end
 end
--- app/serializers/spina/api/page_serializer.rb
+++ app/serializers/spina/api/page_serializer.rb
@@ -1,28 +1,46 @@
-module Spina::Api
-  class PageSerializer < BaseSerializer    
-    set_type :page
-    
-    attributes :title, :seo_title, :menu_title, :materialized_path, :name, :description, :view_template
-    
-    attribute(:content) { |page| page_content(page) }
-    
-    belongs_to :resource
+module Spina
+  module Api
+    class PageSerializer < BaseSerializer
+      set_type :page
 
-    class << self
-      
-      def page_content(page)
-        return [] unless view_template(page)
-        
-        view_template(page)[:parts].map do |part| 
-          { part => page.content(part) }
+      attributes :title, :seo_title, :menu_title, :materialized_path, :name, :description
+
+      attribute(:content) { |page, params| page_content(page, params) }
+
+      belongs_to :resource
+
+      class << self
+        def page_content(page, params)
+          return [] unless view_template(page)
+
+          view_template(page)[:parts].map do |part|
+            if %w[onboarding_logo onboarding_step_image].include?(part)
+              fetch_image_data(params, part, page)
+            else
+              { part => page.content(part) }
+            end
+          end
+        end
+
+        def view_template(page)
+          Spina::Current.theme.view_templates.find { |view_template| view_template[:name] == page.view_template }
+        end
+
+        def image_contents(page_attributes, image, params, resize_key, part)
+          { part => page_attributes.merge(
+            original_url: params[:view_context].main_app.url_for(image.file),
+            thumbnail_url: params[:view_context].main_app.url_for(image.variant(resize_to_fill: Spina.config.thumbnail_image_size)),
+            embedded_image_size_url: params[:view_context].main_app.url_for(image.variant({ resize_key => Spina.config.embedded_image_size }))
+          ) }
+        end
+
+        def fetch_image_data(params, part, page)
+          page_attributes = page.content(part).attributes
+          image = Spina::Image.find(page_attributes['image_id'])
+          resize_key = Spina.config.embedded_image_size.is_a?(Array) ? :resize_to_limit : :resize
+          image_contents(page_attributes, image, params, resize_key, part)
         end
       end
-      
-      def view_template(page)
-        Spina::Current.theme.view_templates.find{|view_template| view_template[:name] == page.view_template}
-      end
-      
     end
-    
   end
 end
